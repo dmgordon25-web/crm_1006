5e6f4fc6126eff13ce7092bbc4e3f5024e28a15f
dmgordon25-web <dmgordon25@gmail.com>
2025-10-09 14:22:55 -0400

    Replace CRM launcher scripts

--- Start-CRM.bat
+++ Start-CRM.bat
@@ -1,8 +1,6 @@
 @echo off
 setlocal
 pushd "%~dp0"
-set "LOGDIR=%LOCALAPPDATA%\CRM\logs"
-if not exist "%LOGDIR%" mkdir "%LOGDIR%" >nul 2>nul
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -NoExit -File ".\tools\Start-CRM.ps1"
 popd
 endlocal
